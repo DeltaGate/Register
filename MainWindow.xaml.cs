c145cf3cdd751f28b18ba6615e628c4a62ac8d87
DeltaGate <connor@conkat.co.uk>
2024-03-30 13:51:54 +0000

    Added Basic Button Functionality
    Added price counter to keep track of transaction value
    
    Added functions to buttons so when pressed they update value of price by appending their diget to current price
    
    Added Delete Key functionality to completely wipe current price
    
    Added Back button to remove last diget
    
    Added Dot as a tempory messure to give penny price

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,14 +20,98 @@ namespace Register
     /// </summary>
     public partial class MainWindow : Window
     {
+        string price;
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
+        
+        // Button Controls
 
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            price = price + btn1.Content;
+            priceCount.Text = price;
+
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            price = price + btn2.Content;
+            priceCount.Text = price;
+        }
+
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+            price = price + btn3.Content;
+            priceCount.Text = price;
+        }
+
+        private void btn4_Click(object sender, RoutedEventArgs e)
+        {
+            price = price + btn4.Content;
+            priceCount.Text = price;
+        }
+
+        private void btn5_Click(object sender, RoutedEventArgs e)
+        {
+            price = price + btn5.Content;
+            priceCount.Text = price;
+        }
+
+        private void btn6_Click(object sender, RoutedEventArgs e)
+        {
+            price = price + btn6.Content;
+            priceCount.Text = price;
+        }
+
+        private void btn7_Click(object sender, RoutedEventArgs e)
+        {
+            price = price + btn7.Content;
+            priceCount.Text = price;
+        }
+
+        private void btn8_Click(object sender, RoutedEventArgs e)
+        {
+            price = price + btn8.Content;
+            priceCount.Text = price;
+        }
+
+        private void btn9_Click(object sender, RoutedEventArgs e)
+        {
+            price = price + btn9.Content;
+            priceCount.Text = price;
+        }
+
+        private void btn0_Click(object sender, RoutedEventArgs e)
+        {
+            price = price + btn0.Content;
+            priceCount.Text = price;
+        }
+
+        private void btnEnter_Click(object sender, RoutedEventArgs e)
+        {
+            price = price + btn0.Content;
+            priceCount.Text = price;
+        }
+
+        private void btnDel_Click(object sender, RoutedEventArgs e)
+        {
+            price = price.Substring(0, 0);
+            priceCount.Text = price;
+        }
+            // btnDot to be removed in favour of auto function in future
+        private void btnDot_Click(object sender, RoutedEventArgs e)
+        {
+            price = price + btnDot.Content;
+            priceCount.Text = price;
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            price = price.Substring(0, price.Length - 1);
+            priceCount.Text = price;
         }
     }
 }
